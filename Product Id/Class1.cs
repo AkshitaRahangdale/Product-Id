7319b2a9d9cd1d289e85e2944a96ba43136b451d
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-10 15:16:29 +0530

    Add project files.

---
+++ Product Id/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Id
+{
+    public class Product
+    {
+        private int id,price;
+        private string name;
+
+        public void Add()
+        {
+            id = 123;
+            name = "Joe";
+            price = 80;
+        }
+
+           public string Print()
+            {
+                return $"product:{id}/{name}/{price}";
+            }
+
+            
+    }
+ }
+    
+        
+    
+
---
+++ Product Id/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Id
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product();
+            p1.Add();
+            Console.WriteLine(p1.Print());
+
+            Product p2 = new Product();
+            p2.Add();
+            Console.WriteLine(p2.Print());
+        }
+    }
+}
